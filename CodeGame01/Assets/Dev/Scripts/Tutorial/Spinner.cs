ef06590b2bc33fa1454079d5c2d72f184c8e55ff
fehmielmas <mimarfehmielmas@gmail.com>
2023-11-25 21:45:13 +0300

    scene

--- CodeGame01/Assets/Dev/Scripts/Tutorial/Spinner.cs
+++ CodeGame01/Assets/Dev/Scripts/Tutorial/Spinner.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class Spinner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float rotateSpeed = 0.5f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0,rotateSpeed,0);
     }
 }
