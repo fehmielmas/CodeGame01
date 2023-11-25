32cd10d9392503e2fa2521a6d246a0c5a1bf12fd
fehmielmas <mimarfehmielmas@gmail.com>
2023-11-25 15:20:27 +0300

    tutorialcodes

---
+++ CodeGame01/Assets/Dev/Scripts/Tutorial/Printtoconsole.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Printtoconsole : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("I'm your man, Sir!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
