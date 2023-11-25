49267c8fb5f342f044b15ad6a8188d9826b89d8e
fehmielmas <mimarfehmielmas@gmail.com>
2023-11-25 16:41:21 +0300

    code

--- CodeGame01/Assets/Dev/Scripts/Tutorial/PlayerMovement.cs
+++ CodeGame01/Assets/Dev/Scripts/Tutorial/PlayerMovement.cs
@@ -7,12 +7,14 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform.Translate(1,0,0);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime;
+        transform.Translate(xValue,0,zValue);
     }
 }
