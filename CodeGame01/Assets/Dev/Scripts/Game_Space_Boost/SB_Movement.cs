8fe4e794ee55376d3e9f37d34881c53ebce34a0a
fehmielmas <mimarfehmielmas@gmail.com>
2023-11-26 15:39:26 +0300

    scene

--- CodeGame01/Assets/Dev/Scripts/Game_Space_Boost/SB_Movement.cs
+++ CodeGame01/Assets/Dev/Scripts/Game_Space_Boost/SB_Movement.cs
@@ -7,24 +7,48 @@ using Debug = UnityEngine.Debug;
 
 public class SB_Movement : MonoBehaviour
 {
+    private Rigidbody rb; 
+    [SerializeField] private float rotateSpeed = 100f;
+    [SerializeField] private float mainThrust = 200f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        ProcessThrust();
+        ProcessRotation();
     }
 
-    void ProcessInput()
+    void ProcessThrust()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            Debug.Log("pressed space");
+            rb.freezeRotation = true;
+            rb.AddRelativeForce(Vector3.up * Time.deltaTime * mainThrust);
+            rb.freezeRotation = false;
+        }
+        
+    }
+
+    void ProcessRotation()
+    {
+        if (Input.GetKey((KeyCode.A)))
+        {
+            ApplyRotation(rotateSpeed);
+        }
+        else if (Input.GetKey((KeyCode.D)))
+        {
+            ApplyRotation(-rotateSpeed);
         }
     }
 
+    public void ApplyRotation(float rotationThisFrame)
+    {
+        transform.Rotate(Vector3.forward * Time.deltaTime * rotationThisFrame);
+    }
 }
