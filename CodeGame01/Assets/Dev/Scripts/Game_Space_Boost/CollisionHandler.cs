aaf1436a392c4d2e33ee74a49be9c176dec6aa1e
fehmielmas <mimarfehmielmas@gmail.com>
2023-11-26 20:43:05 +0300

    as

--- CodeGame01/Assets/Dev/Scripts/Game_Space_Boost/CollisionHandler.cs
+++ CodeGame01/Assets/Dev/Scripts/Game_Space_Boost/CollisionHandler.cs
@@ -1,8 +1,11 @@
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
+
 public class CollisionHandler : MonoBehaviour
 {
+    [FormerlySerializedAs("levelLoadEffect")] public float levelLoadDelay = 0.5f;
     void OnCollisionEnter(Collision other)
     {
         switch (other.gameObject.tag)
@@ -11,20 +14,35 @@ public class CollisionHandler : MonoBehaviour
                 Debug.Log("this thing is friendly");
                 break;
             case "Finish":
-                NextLevel();
-                break;
-            case "Fuel":
-                Debug.Log("More Fuell yeahhh");
+                StartSuccessSequence();
                 break;
             case "Enemy":
-                ReloadLevel();
+                StartCrashSequence();
                 break;
         }
     }
-
-    void NextLevel()
+    void StartSuccessSequence()
     {
-        
+        GetComponent<SB_Movement>().enabled = false;
+        Invoke("LoadNextLevel", levelLoadDelay);
+    }
+
+    void StartCrashSequence()
+    {
+        GetComponent<SB_Movement>().enabled = false;
+        Invoke("ReloadLevel", levelLoadDelay);
+    }
+
+    void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+        SceneManager.LoadScene(currentSceneIndex + 1);
     }
 
     void ReloadLevel()
