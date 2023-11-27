87a2e7154982da43cc97b3d503bcd26ac7211a03
fehmielmas <mimarfehmielmas@gmail.com>
2023-11-27 20:18:23 +0300

    scene

---
+++ CodeGame01/Assets/Dev/Scripts/Game_Space_Boost/Oscilate2.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscilate2 : MonoBehaviour
+{
+    private Vector3 objectPosition;
+    [SerializeField] private Vector3 objecyMovementPosition;
+    [SerializeField] [Range(0,1)] private float objectMovementFactor;
+    [SerializeField] private float period = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        objectPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float cycles = Time.time / period;
+        const float tau = Mathf.PI * 2f;
+        float rawSinWave = Mathf.Sin(cycles * tau);
+
+        objectMovementFactor = (rawSinWave + 1f) / 2f;
+            
+        Vector3 offset = objectMovementFactor * objecyMovementPosition;
+        transform.position = objectPosition + offset;
+    }
+}
