d16329f882fce3e4dff4f28980bdab425dbda726
fehmielmas <mimarfehmielmas@gmail.com>
2023-11-27 20:25:34 +0300

    as

--- CodeGame01/Assets/Dev/Scripts/Game_Space_Boost/Oscilate2.cs
+++ CodeGame01/Assets/Dev/Scripts/Game_Space_Boost/Oscilate2.cs
@@ -17,7 +17,7 @@ public class Oscilate2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (period == 0) {return;}
+        if (period == 0f) {return;}
         float cycles = Time.time / period;
         const float tau = Mathf.PI * 2f;
         float rawSinWave = Mathf.Sin(cycles * tau);
--- CodeGame01/Assets/Dev/Scripts/Game_Space_Boost/Oscillator.cs
+++ CodeGame01/Assets/Dev/Scripts/Game_Space_Boost/Oscillator.cs
@@ -19,7 +19,7 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (period == 0) {return;}
+        if (period == 0f) {return;}
         float cycles = Time.time / period;
         const float tau = Mathf.PI * 2;
         float rawSinWave = Mathf.Sin(cycles * tau);
