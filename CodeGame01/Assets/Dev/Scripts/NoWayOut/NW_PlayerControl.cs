f72b4c05fb65446e92aeaa767f90da99d06554cb
fehmielmas <mimarfehmielmas@gmail.com>
2023-12-04 20:09:07 +0300

    s

--- CodeGame01/Assets/Dev/Scripts/NoWayOut/NW_PlayerControl.cs
+++ CodeGame01/Assets/Dev/Scripts/NoWayOut/NW_PlayerControl.cs
@@ -1,21 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEngine.Serialization;
 
 public class NW_PlayerControl : MonoBehaviour
 {
-    [SerializeField] private InputAction movement;
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    [SerializeField] private float controlSpeed = 5f;
     // Update is called once per frame
     void Update()
     {
-        
+        float xAxisThrow = Input.GetAxis("Horizontal");
+        float yAxisThrow = Input.GetAxis("Vertical");
+
+        float xOffset = xAxisThrow * Time.deltaTime * controlSpeed;
+        float newXPos = transform.localPosition.x + xOffset;
+        transform.localPosition = new Vector3(newXPos, transform.localPosition.y, transform.localPosition.z);
     }
+
 }
